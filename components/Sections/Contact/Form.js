4d13a8996db571dcdef4e0ef7c27688ca53cfbfc
tomasyovino <tomasyovino@gmail.com>
2022-12-02 01:08:16 -0300

    Adds Contact component: Structure, design & responsive

--- components/Sections/Contact/Form.js
+++ components/Sections/Contact/Form.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <form className='contact-form' id='contact-form'>
+      <h4>Message Me</h4>
+      <div className='row'>
+        <div className='col-12 col-md-6 form-group'>
+          <input className='form-control' id='contact-name' type='text' name='name' placeholder='Name' />
+        </div>
+        <div className='col-12 col-md-6 form-group'>
+          <input className='form-control' id='contact-email' type='email' name='email' placeholder='Email' />
+        </div>
+        <div className='col-12 form-group'>
+          <input className='form-control' id='contact-subject' type='text' name='subject' placeholder='Subject' />
+        </div>
+        <div className='col-12 form-group form-message'>
+          <textarea className='form-control' id='contact-message' name='message' placeholder='Message' rows='5' />
+        </div>
+        <div className='col-12 form-submit'>
+          <button className='btn button-main button-scheme' id='contact-submit' type='submit'>Send Message</button>
+          <p className='contact-feedback' style={{display: "none"}}></p>
+        </div>
+      </div>
+    </form>
   )
 }
 
--- components/Sections/Contact/Info.js
+++ components/Sections/Contact/Info.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import { Code, Map, Call, Send } from '@mui/icons-material';
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className='contact-info'>
+      <h4>Contact Info</h4>
+      <p>Always available for freelance work if the right project comes along, Feel free to contact me!</p>
+      <ul className='list-unstyled list-info'>
+        <li>
+          <div className='media align-items-center'>
+            <Code className='info-icon' />
+            <div className='media-body info-details'>
+              <h6>Name</h6>
+              <span>Tomás Yovino</span>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='media align-items-center'>
+            <Map className='info-icon' />
+            <div className='media-body info-details'>
+              <h6>Dirección</h6>
+              <span>Lanús Oeste, Buenos Aires, Argentina</span>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='media align-items-center'>
+            <Call className='info-icon' />
+            <div className='media-body info-details'>
+              <h6>Call Me</h6>
+              <a href='#'><span>+54 9 113911 2892</span></a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='media align-items-center'>
+            <Send className='info-icon' />
+            <div className='media-body info-details'>
+              <h6>Email Me</h6>
+              <a href='#'><span>tomasyovino@gmail.com</span></a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
 
